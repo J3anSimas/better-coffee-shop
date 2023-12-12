d89575ceb560bd7752e0ae8dbdc9cec2cb0b6657
= <=>
2023-12-12 18:01:12 -0300

    Fixing sql syntax

--- create.sql
+++ create.sql
@@ -1,51 +1,50 @@
-create table user (
-	id varchar(36) primary key,
-    name varchar(255) not null,
-    auth0_id varchar(30) not null
+CREATE TABLE USERS (
+	ID VARCHAR(36) PRIMARY KEY,
+    NAME VARCHAR(255) NOT NULL,
+    AUTH0_ID VARCHAR(30) NOT NULL
     
 );
-create table product (
-	id varchar(36) primary key,
-    description varchar(255) primary key,
-    price integer not null,
-    created_at timestamp default CURRENT_TIMESTAMP
+CREATE TABLE PRODUCTS (
+	ID VARCHAR(36) PRIMARY KEY,
+    DESCRIPTION VARCHAR(255),
+    PRICE INTEGER NOT NULL,
+    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-create table cart (
-	id varchar(36) primary key,
-    user_id varchar(36) not null,
-    created_at timestamp default CURRENT_TIMESTAMP
+CREATE TABLE CARTS (
+	ID VARCHAR(36) PRIMARY KEY,
+    USER_ID VARCHAR(36) NOT NULL,
+    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
-create table cart_item (
-	id varchar(36) primary key,
-    cart_id varchar(36) not null,
-    product_id varchar(36) not null,
-    quantity integer not null,
-    created_at timestamp default CURRENT_TIMESTAMP
+CREATE TABLE CART_ITEM (
+	ID VARCHAR(36) PRIMARY KEY,
+    CART_ID VARCHAR(36) NOT NULL,
+    PRODUCT_ID VARCHAR(36) NOT NULL,
+    QUANTITY INTEGER NOT NULL,
+    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 );
-create table payment_method (
-    id varchar(36) primary key,
-    description varchar(255) not null
+CREATE TABLE PAYMENT_METHODS (
+    ID VARCHAR(36) PRIMARY KEY,
+    DESCRIPTION VARCHAR(255) NOT NULL
 
 );
-)
-create table order (
-    id varchar(36) primary key,
-    user_id varchar(36) not null,
-    payment_id varchar(36) not null,
-    created_at timestamp default CURRENT_TIMESTAMP
+CREATE TABLE ORDERS (
+    ID VARCHAR(36) PRIMARY KEY,
+    USER_ID VARCHAR(36) NOT NULL,
+    PAYMENT_ID VARCHAR(36) NOT NULL,
+    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-create table order_item (
-    id varchar(36) primary key,
-    order_id varchar(36) not null,
-    product_id varchar(36) not null,
-    quantity integer not null,
-    price integer not null
+CREATE TABLE ORDER_ITEM (
+    ID VARCHAR(36) PRIMARY KEY,
+    ORDER_ID VARCHAR(36) NOT NULL,
+    PRODUCT_ID VARCHAR(36) NOT NULL,
+    QUANTITY INTEGER NOT NULL,
+    PRICE INTEGER NOT NULL
 );
-alter table cart add constraint fk_cart_user foreign key (user_id) references user(id);
-alter table cart_item add constraint fk_cart_item_cart foreign key (cart_id) references cart(id);
-alter table cart_item add constraint fk_cart_item_product foreign key (product_id) references product(id);
-alter table order add constraint fk_order_user foreign key (user_id) references user(id);
-alter table order add constraint fk_order_payment foreign key (payment_id) references payment_method(id);
-alter table order_item add constraint fk_order_item_order foreign key (order_id) references order(id);
-alter table order_item add constraint fk_order_item_product foreign key (product_id) references product(id);
+ALTER TABLE CARTS ADD CONSTRAINT FK_CART_USER FOREIGN KEY (USER_ID) REFERENCES USERS(ID);
+ALTER TABLE CART_ITEMS ADD CONSTRAINT FK_CART_ITEM_CART FOREIGN KEY (CART_ID) REFERENCES CARTS(ID);
+ALTER TABLE CART_ITEMS ADD CONSTRAINT FK_CART_ITEM_PRODUCT FOREIGN KEY (PRODUCT_ID) REFERENCES PRODUCTS(ID);
+ALTER TABLE ORDERS ADD CONSTRAINT FK_ORDER_USER FOREIGN KEY (USER_ID) REFERENCES USERS(ID);
+ALTER TABLE ORDERS ADD CONSTRAINT FK_ORDER_PAYMENT FOREIGN KEY (PAYMENT_ID) REFERENCES PAYMENT_METHODS(ID);
+ALTER TABLE ORDER_ITEMS ADD CONSTRAINT FK_ORDER_ITEM_ORDER FOREIGN KEY (ORDER_ID) REFERENCES ORDERS(ID);
+ALTER TABLE ORDER_ITEMS ADD CONSTRAINT FK_ORDER_ITEM_PRODUCT FOREIGN KEY (PRODUCT_ID) REFERENCES PRODUCTS(ID);
